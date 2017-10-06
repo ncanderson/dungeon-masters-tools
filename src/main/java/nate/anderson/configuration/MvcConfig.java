567df05ae8902a8ca7e25c2b2dde05ff730943af
Nate Anderson <nathanaelcanderson@gmail.com>
2017-10-06 08:02:06 -0400

    add spring security, reconigure view path config since I realized what thymeleaf is

--- src/main/java/nate/anderson/configuration/MvcConfig.java
+++ src/main/java/nate/anderson/configuration/MvcConfig.java
@@ -1,17 +1,30 @@
 package nate.anderson.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "nate.anderson")
 public class MvcConfig extends WebMvcConfigurerAdapter{
-
+//
+//	@Bean
+//	public ViewResolver internalResourceViewResolver() {
+//	    InternalResourceViewResolver bean = new InternalResourceViewResolver();
+//	    bean.setViewClass(JstlView.class);
+//	    bean.setPrefix("/WEB-INF/views/");
+//	    bean.setSuffix(".jsp");
+//	    return bean;
+//	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry
@@ -21,4 +34,5 @@ public class MvcConfig extends WebMvcConfigurerAdapter{
 	      .resourceChain(true)
 	      .addResolver(new PathResourceResolver());
 	}
+	
 }
