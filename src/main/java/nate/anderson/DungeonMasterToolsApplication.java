ce0a8d050877d2bb626af618fa43be63ef5347d6
Nate Anderson <nathanaelcanderson@gmail.com>
2017-10-09 20:59:56 -0400

    restart, rebuild

---
+++ src/main/java/nate/anderson/DungeonMasterToolsApplication.java
@@ -0,0 +1,12 @@
+package nate.anderson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonMasterToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonMasterToolsApplication.class, args);
+	}
+}
--- src/main/java/nate/anderson/controller/HomeController.java
+++ src/main/java/nate/anderson/controller/HomeController.java
@@ -1,16 +1,15 @@
 package nate.anderson.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+	@RequestMapping(value="/", method=RequestMethod.GET)
     public String greeting() {
         return "index";
     }
-
+	
 }
