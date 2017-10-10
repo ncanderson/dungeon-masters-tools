7fa84c5b8d8317c20bf5aae17e3beec202711f81
Nate Anderson <nathanaelcanderson@gmail.com>
2017-10-09 22:15:00 -0400

    add REST controller

---
+++ src/main/java/nate/anderson/controller/RESTFULControllers.java
@@ -0,0 +1,15 @@
+package nate.anderson.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RESTFULControllers {
+
+	
+	public String initNewTownConstruction() {
+		
+		return "test";
+		
+	}
+	
+}
