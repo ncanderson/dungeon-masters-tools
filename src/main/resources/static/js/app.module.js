88d8f500d7f5b0feba13dc5bdea0f80e05b54b5e
Nate Anderson <nathanaelcanderson@gmail.com>
2018-01-23 07:22:10 -0500

    add ui-routere

--- src/main/resources/static/js/app.module.js
+++ src/main/resources/static/js/app.module.js
@@ -1,7 +1,39 @@
 var dungeonMastersTools = angular.module('dungeonMastersTools', [
 	'randomTownGenerator.module',
-	'mainNavigation.module'
+	'mainNavigation.module',
+	'ui.router'
 ]);
+
+dungeonMastersTools.config([
+	'$stateProvider', 
+	'$urlRouterProvider', 
+	
+	function($stateProvider, $urlRouterProvider) {
+	    $urlRouterProvider.otherwise('/');
+	
+		var home = {
+			name: 'home',
+			url: '/',
+			template: '<div class="jumbotron">' + 
+					  	  '<h1 class="display-3">Home Page</h1>' +
+						  '<hr class="my-4">' +
+						  '<p>A collection of tools for Dungeon Masters</p>' +
+						  '<p class="lead">' +
+						  '</p>' +
+				  	  '</div>'
+		}
+	
+		var randomTownGenerator = {
+		    name: 'randomTownGenerator',
+		    url: '/random-town',
+		    component: 'randomTownGenerator'
+		}
+		
+		$stateProvider.state(home);
+		$stateProvider.state(randomTownGenerator);
+	
+	}
+]);	
 
 dungeonMastersTools.controller('MainCtrl', [
 	
